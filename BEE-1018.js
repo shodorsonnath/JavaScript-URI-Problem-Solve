b46bbe5bf7547e10576afe1a52e076820ef59a16
shodorsonnath <shodorsonnath@gmail.com>
2023-08-09 00:49:02 +0600

    BEE-1018

--- BEE-1018.js
+++ BEE-1018.js
@@ -1,1 +1,16 @@
 // This is a simple javascript problem form beecrowd
+let n = 576;
+console.log(n,'\n');
+console.log(parseInt(n/100),'nota(s) de R$ 100,00\n');
+n = n % 100;
+console.log(parseInt(n/50),'nota(s) de R$ 50,00\n');
+n = n % 50;
+console.log(parseInt(n/20),'nota(s) de R$ 20,00\n');
+n = n % 20;
+console.log(parseInt(n/10),'nota(s) de R$ 10,00\n');
+n = n % 10;
+console.log(parseInt(n/5),'nota(s) de R$ 5,00\n');
+n = n % 5;
+console.log(parseInt(n/2),'nota(s) de R$ 2,00\n');
+n = n % 2;
+console.log(parseInt(n),'nota(s) de R$ 1,00\n');
